7ec81bcf827769e59592e46ab4ab73c8d82f7b6b
Oscar <voscarmv@gmail.com>
2021-02-17 21:33:32 -0600

    Added custom middleware.

---
+++ src/middleware/index.js
@@ -0,0 +1,15 @@
+import ADD_ARTICLE from '../actions/action-types';
+
+const forbiddenWordsMiddleware = store => next => action => {
+  const forbiddenWords = ['spam', 'money'];
+  if (action.type === ADD_ARTICLE) {
+    const foundWord = forbiddenWords.filter(word => action.payload.title.includes(word));
+
+    if (foundWord.length) {
+      return store.dispatch({ type: 'FOUND_BAD_WORD' });
+    }
+  }
+  return next(action);
+};
+
+export default forbiddenWordsMiddleware;
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import forbiddenWordsMiddleware from '../middleware/index';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      logger,
+      forbiddenWordsMiddleware,
+    ),
+  ),
+);
 
 export default store;
